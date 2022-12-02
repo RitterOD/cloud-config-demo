7296bbd1c799d328c2efb7bda9036f0b24503251
Alexander Maslov <ritterod@gmail.com>
2022-12-02 09:57:47 +0300

    cloud-config-demo: Initial commit
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ config-server/src/test/java/org/maslov/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maslov.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ demo-service/src/test/java/org/maslov/template/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maslov.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
