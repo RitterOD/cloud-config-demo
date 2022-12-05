864179771f9bb80d0bdcf428d64897e8b383e640
Alexander Maslov <ritterod@gmail.com>
2022-12-05 09:11:35 +0300

    cloud-config-demo: fix configuration
    1) remove application.properties
    2) add expositions of spring boot actuator endpoints in client
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- demo-service/src/main/java/org/maslov/template/controller/AppController.java
+++ demo-service/src/main/java/org/maslov/template/controller/AppController.java
@@ -1,12 +1,14 @@
 package org.maslov.template.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class AppController {
     @Value("${user.role}")
     private String role;
